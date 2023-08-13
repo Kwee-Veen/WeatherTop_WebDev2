681b6775c1196e268dcba0d56bc81a92fb309579
Glitch (weathertop-ca) <none>
2023-08-13 17:11:31 +0000

    Push #3b: Station view, Reading creation display & deletion

--- routes.js
+++ routes.js
@@ -1,27 +1,26 @@
 import express from "express";
 import { indexController } from "./controllers/index-controller.js";
-// import { dashboardController } from "./controllers/dashboard-controller.js";
+import { dashboardController } from "./controllers/dashboard-controller.js";
 // import { aboutController } from "./controllers/about-controller.js";
-// import { playlistController } from "./controllers/playlist-controller.js";
-// import { accountsController } from "./controllers/accounts-controller.js";
+import { stationController } from "./controllers/station-controller.js";
+import { accountsController } from "./controllers/accounts-controller.js";
 // import { trackController } from "./controllers/track-controller.js";
 
 export const router = express.Router();
 
 
 router.get("/", indexController.index);
-// router.get("/login", accountsController.login);
-// router.get("/signup", accountsController.signup);
-// router.get("/logout", accountsController.logout);
-// router.post("/register", accountsController.register);
-// router.post("/authenticate", accountsController.authenticate);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
 // router.get("/about", aboutController.index);
-// router.get("/dashboard", dashboardController.index);
-// router.post("/dashboard/addplaylist", dashboardController.addPlaylist);
-// router.get("/dashboard/deleteplaylist/:id", dashboardController.deletePlaylist);
-// router.get("/playlist/:id", playlistController.index);
-// router.get("/playlist/:id", playlistController.index);
-// router.post("/playlist/:id/addtrack", playlistController.addTrack);
-// router.get("/playlist/:playlistid/deletetrack/:trackid", playlistController.deleteTrack)
+router.get("/dashboard", dashboardController.index);
+router.post("/dashboard/addstation", dashboardController.addStation);
+router.get("/dashboard/deletestation/:id", dashboardController.deleteStation);
+router.get("/station/:id", stationController.index);
+router.post("/station/:id/addreading", stationController.addReading);
+router.get("/station/:stationid/deletereading/:readingid", stationController.deleteReading)
 // router.get("/playlist/:playlistid/edittrack/:trackid", trackController.index);
 // router.post("/playlist/:playlistid/updatetrack/:trackid", trackController.update);
