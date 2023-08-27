553c9e4fd544788a9c2257c9c38253230e7ef176
Glitch (weathertop-ca) <none>
2023-08-27 22:45:13 +0000

    V11: Station trend table.

--- controllers/station-controller.js
+++ controllers/station-controller.js
@@ -11,11 +11,30 @@ export const stationController = {
     let station = [];
     station[0] = await stationStore.getStationById(request.params.id);
     let readings = await stationStore.getStationByIdWithReadings(request.params.id);
+    const lat = station[0].latitude;
+    const long = station[0].longitude;
+    const apiKey = "0c109ad6bb8a0b5d8a284ce6061f12c6";
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=` + lat + `&lon=` + long + `&appid=` + apiKey;
+    const search = await fetch(url);
+    const data = await search.json();
+    let trends = {
+      tempTrend: [],
+      trendLabels: [],
+    };
+    let j = 0;
+    for (let i = 4; i < data.list.length; i += 8) {
+      let tempTrend = await dataConversions.rounder((data.list[i].main.temp)-273.15);
+      trends.tempTrend.push(tempTrend);
+      const date = new Date(data.list[i].dt * 1000);
+      trends.trendLabels.push(`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`);
+      j++;
+    }
     const viewData = {
       title: station.title,
       station: station,
       readings: readings.readings,
       stationid: station._id,
+      trends: trends
     };
     console.log(`Rendering station view`);
     response.render("station-view", viewData);
@@ -46,17 +65,19 @@ export const stationController = {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=` + lat + `&lon=` + long + `&appid=` + apiKey;
     const search = await fetch(url);
     const data = await search.json();
-    let autoReading = {
+    let autoReading = undefined;
+    if (data != undefined) {
+      autoReading = {
       code: data.weather[0].id,
       temperature: await dataConversions.rounder(data.main.temp-273.15),
       windSpeed: await dataConversions.rounder(data.wind.speed),
       windDirection: await dataConversions.rounder(data.wind.deg),
       pressure: await dataConversions.rounder(data.main.pressure),
       time: now.toLocaleString('en-GB', { timeZone: 'UTC' }),
+      }
     }
-    console.log(data);
     await readingStore.addReading(station._id, autoReading);
-    console.log(`Generating automatic reading`);
+    console.log(`Generating automatic reading at time ${autoReading.time}`);
     response.redirect("/station/" + station._id);
   },
   
